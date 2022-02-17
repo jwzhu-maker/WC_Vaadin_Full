45dc750abb060c457fa624ac06dc3b1eafef88ab
start.vaadin.com <no-reply@vaadin.com>
2022-02-17 10:14:43 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,182 @@
+package com.example.application.views;
+
+
+import com.example.application.views.about.AboutView;
+import com.example.application.views.cardlist.CardListView;
+import com.example.application.views.chat.ChatView;
+import com.example.application.views.checkoutform.CheckoutFormView;
+import com.example.application.views.creditcardform.CreditCardFormView;
+import com.example.application.views.dashboard.DashboardView;
+import com.example.application.views.editor.EditorView;
+import com.example.application.views.helloworld.HelloWorldView;
+import com.example.application.views.imagelist.ImageListView;
+import com.example.application.views.list.ListView;
+import com.example.application.views.map.MapView;
+import com.example.application.views.masterdetail.MasterDetailView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+@PWA(name = "My App", shortName = "My App", enableInstallPrompt = false)
+@Push
+@Theme(themeFolder = "myapp")
+public class MainLayout extends AppLayout {
+
+    /**
+     * A simple navigation item component, based on ListItem element.
+     */
+    public static class MenuItemInfo extends ListItem {
+
+        private final Class<? extends Component> view;
+
+        public MenuItemInfo(String menuTitle, String iconClass, Class<? extends Component> view) {
+            this.view = view;
+            RouterLink link = new RouterLink();
+            // Use Lumo classnames for various styling
+            link.addClassNames("flex", "mx-s", "p-s", "relative", "text-secondary");
+            link.setRoute(view);
+
+            Span text = new Span(menuTitle);
+            // Use Lumo classnames for various styling
+            text.addClassNames("font-medium", "text-s");
+
+            link.add(new LineAwesomeIcon(iconClass), text);
+            add(link);
+        }
+
+        public Class<?> getView() {
+            return view;
+        }
+
+        /**
+         * Simple wrapper to create icons using LineAwesome iconset. See
+         * https://icons8.com/line-awesome
+         */
+        @NpmPackage(value = "line-awesome", version = "1.3.0")
+        public static class LineAwesomeIcon extends Span {
+            public LineAwesomeIcon(String lineawesomeClassnames) {
+                // Use Lumo classnames for suitable font size and margin
+                addClassNames("me-s", "text-l");
+                if (!lineawesomeClassnames.isEmpty()) {
+                    addClassNames(lineawesomeClassnames);
+                }
+            }
+        }
+
+    }
+
+    private H1 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
+    }
+
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassName("text-secondary");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("m-0", "text-l");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "h-xl", "items-center",
+                "w-full");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("My App");
+        appName.addClassNames("flex", "items-center", "h-xl", "m-0", "px-m", "text-m");
+
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
+                createNavigation(), createFooter());
+        section.addClassNames("flex", "flex-col", "items-stretch", "max-h-full", "min-h-full");
+        return section;
+    }
+
+    private Nav createNavigation() {
+        Nav nav = new Nav();
+        nav.addClassNames("border-b", "border-contrast-10", "flex-grow", "overflow-auto");
+        nav.getElement().setAttribute("aria-labelledby", "views");
+
+        // Wrap the links in a list; improves accessibility
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (MenuItemInfo menuItem : createMenuItems()) {
+            list.add(menuItem);
+
+        }
+        return nav;
+    }
+
+    private MenuItemInfo[] createMenuItems() {
+        return new MenuItemInfo[]{ //
+                new MenuItemInfo("Hello World", "la la-globe", HelloWorldView.class), //
+
+                new MenuItemInfo("About", "la la-file", AboutView.class), //
+
+                new MenuItemInfo("Image List", "la la-th-list", ImageListView.class), //
+
+                new MenuItemInfo("Dashboard", "la la-chart-area", DashboardView.class), //
+
+                new MenuItemInfo("List", "la la-th", ListView.class), //
+
+                new MenuItemInfo("Card List", "la la-list", CardListView.class), //
+
+                new MenuItemInfo("Credit Card Form", "la la-credit-card", CreditCardFormView.class), //
+
+                new MenuItemInfo("Master-Detail", "la la-columns", MasterDetailView.class), //
+
+                new MenuItemInfo("Checkout Form", "la la-credit-card", CheckoutFormView.class), //
+
+                new MenuItemInfo("Map", "la la-map", MapView.class), //
+
+                new MenuItemInfo("Chat", "la la-comments", ChatView.class), //
+
+                new MenuItemInfo("Editor", "la la-edit", EditorView.class), //
+
+        };
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("flex", "items-center", "my-s", "px-m", "py-xs");
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
